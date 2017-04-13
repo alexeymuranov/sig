4562dd00c4d6697527ed0b15147685744691b8f4
Alexey Muranov <alexeymuranov@users.noreply.github.com>
2017-04-13 13:04:16 +0200

    clearly base run-sig on __main__

--- src/run-sig.py
+++ src/run-sig.py
@@ -1,20 +1,26 @@
 #!/usr/bin/env python3
 
-# NOTE:  This startup script is here for demonstration.  Consider using
+# -----------------------------------
+# Startup script based on __main__.py
+# -----------------------------------
 #
-#       python3 -m sig
+# This startup script is here for demonstration.  Consider using
 #
-#   instead of
+#     python3 -m sig
 #
-#       ./run-sig.py
+# instead of
+#
+#     ./run-sig.py
 #
 # NOTE:  With Setuptools the package can be configured for installation
 #   with pip so that installing the package will automatically generate
 #   and install a platform-specific startup executable.
 
-from sig.cli import main
 from sys import argv
 # NOTE: `exit` already means something in Python
 from sys import exit as sys_exit
+from sig.cli import main
 
+# NOTE: `sys.exit` is useless here if `main` calls it anyway, but
+#   harmless and normally expected
 sys_exit(main(argv[1:]))
